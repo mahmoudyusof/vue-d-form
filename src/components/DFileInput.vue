758d5c95873834368c0c5bd215fb97d844cfc1e1
mahmoudyusof <mahmoud.yusof27@gmail.com>
2020-03-06 22:35:13 +0200

    boilerplate for file inputs

---
+++ src/components/DFileInput.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        value: {
+            default: null
+        }
+    },
+    model: {
+        event: "change"
+    },
+    render(h){
+        return h("input", {
+            type: "file",
+            value: this.value,
+            name: this.name,
+            ...this.$attrs
+        })
+    }
+}
+</script>
