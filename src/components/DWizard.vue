a4e4012039f2370d166ca25c31be1b9da7171c0e
mahmoudyusof <mahmoud.yusof27@gmail.com>
2020-04-03 22:07:49 +0200

    initial thought for a wizard

---
+++ src/components/DWizard.vue
@@ -0,0 +1,20 @@
+<script>
+import DForm from "./DForm.vue";
+export default {
+  components: { DForm },
+  props: {
+    steps: {
+      type: Array,
+      required: true
+    },
+    title: {
+      type: String
+    }
+  },
+  render(h) {
+    const forms = steps.map(step =>
+      h("div", [h("DForm", { props: step })], { attrs: { class: "step" } })
+    );
+  }
+};
+</script>
